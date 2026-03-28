fb00f675d94b43b769f67ce2f500bdf1bfd7a089
JoaoOdara <joao@alfajoresodara.com.br>
2026-03-28 12:24:38 -0300

    Update and rename config.js to js/config.js

--- js/config.js
+++ js/config.js
@@ -9,4 +9,4 @@ const SUPABASE_URL = localStorage.getItem('sb-url-override')
   || 'https://khqbimmcibutfrfmkoxr.supabase.co';
 
 const SUPABASE_KEY = localStorage.getItem('sb-key-override')
-  || 'YOUR_SUPABASE_ANON_KEY';   // ← substitua esta linha com sua chave real
+  || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtocWJpbW1jaWJ1dGZyZm1rb3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNTY1NjUsImV4cCI6MjA4OTkzMjU2NX0.w3vRAPhiU7Zavgkiv-ldjbHc_UJeGH9ck2tq_YN6MBo';   // ← substitua esta linha com sua chave real
